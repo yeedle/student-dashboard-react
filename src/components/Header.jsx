a166bb7fdd5dc8a987d2dcead67157ba59794f41
Abraham Neuwirth <abeneuwirth@gmail.com>
2025-08-20 13:10:54 -0400

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,7 @@
 import { useAssignments } from "../context/AssignmentsContext";
 
 const Header = () => {
-  const { assignments } = useAssignments();
-  const totalAssignments = assignments.length;
-  const completedAssignments = assignments.filter(
-    (assignment) => assignment.done
-  ).length;
-  const percentageCompleted = Math.round((completedAssignments / totalAssignments) * 100);
-
-  document.title = `VP Dashboard - ${percentageCompleted}% Completed`;
+  document.title = `VP Dashboard - 0% Completed`;
 
   return (
     <>
